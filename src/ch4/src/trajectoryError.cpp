f525b9167ec91e5648644d490dbb41b40471ae0e
liuiu <liu.moon.710@gmail.com>
2024-10-14 11:49:36 +0800

    add traj with bug

---
+++ src/ch4/src/trajectoryError.cpp
@@ -0,0 +1,113 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sophus/se3.hpp>
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace Eigen;
+
+string groundtruth_file = "src/ch4/src/estimated.txt";
+string estimated_file = "src/ch4/src/groundtruth.txt";
+
+
+class SophusExampleNode : public rclcpp::Node {
+public:
+    SophusExampleNode() : Node("sophus_example_node") {
+
+        // 创建发布器，发布轨迹信息
+        path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("robot_path", 10);
+
+
+        cout << "*******************************" << endl;
+        // 读取轨迹文件
+        vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> groundtruth = ReadTrajectory(groundtruth_file);
+        if (groundtruth.empty()) {
+            RCLCPP_ERROR(this->get_logger(), "groundtruth_file file is empty or cannot be found.");
+            return;
+        }
+
+        // 读取轨迹文件
+        vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> estimated = ReadTrajectory(estimated_file);
+        if (estimated.empty()) {
+            RCLCPP_ERROR(this->get_logger(), "estimated_file file is empty or cannot be found.");
+            return;
+        }
+
+        // compute rmse
+        rmse = 0;
+        for (size_t i = 0; i < estimated.size(); i++) {
+            Sophus::SE3d p1 = estimated[i], p2 = groundtruth[i];
+            double error = (p2.inverse() * p1).log().norm();
+            rmse += error * error;
+        }
+        rmse = rmse / double(estimated.size());
+        rmse = sqrt(rmse);
+        cout << "RMSE = " << rmse << endl;
+
+        // 发布轨迹
+        PublishPath(groundtruth);
+        PublishPath(estimated);
+    }
+    double rmse;
+private:
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
+
+    vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> ReadTrajectory(const string &file_path) {
+        vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> poses;
+        ifstream fin(file_path);
+        if (!fin) {
+            RCLCPP_ERROR(this->get_logger(), "Cannot find trajectory file at %s", file_path.c_str());
+            return poses;
+        }
+
+        while (!fin.eof()) {
+            double time, tx, ty, tz, qx, qy, qz, qw;
+            fin >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
+            Sophus::SE3d Twr(Quaterniond(qw, qx, qy, qz),Vector3d(tx,ty,tz));
+            poses.push_back(Twr);
+        }
+        RCLCPP_INFO(this->get_logger(), "Read total %lu pose entries", poses.size());
+        return poses;
+    }
+
+    void PublishPath(const vector<Sophus::SE3d, Eigen::aligned_allocator<Sophus::SE3d>> &poses) {
+        nav_msgs::msg::Path path_msg;
+        path_msg.header.stamp = this->get_clock()->now();
+        path_msg.header.frame_id = "map";  // 假设我们在“map”坐标系中
+
+        for (const auto &pose : poses) {
+            geometry_msgs::msg::PoseStamped pose_stamped;
+            pose_stamped.header.stamp = this->get_clock()->now();
+            pose_stamped.header.frame_id = "map";
+
+            // 设置位置
+            pose_stamped.pose.position.x = pose.translation().x();
+            pose_stamped.pose.position.y = pose.translation().y();
+            pose_stamped.pose.position.z = pose.translation().z();
+
+            // 设置姿态
+            Quaterniond q(pose.so3().unit_quaternion());
+            pose_stamped.pose.orientation.x = q.x();
+            pose_stamped.pose.orientation.y = q.y();
+            pose_stamped.pose.orientation.z = q.z();
+            pose_stamped.pose.orientation.w = q.w();
+
+            path_msg.poses.push_back(pose_stamped);
+        }
+
+        // 发布路径
+        path_publisher_->publish(path_msg);
+    }
+};
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SophusExampleNode>());
+    rclcpp::shutdown();
+    return 0;
+}
--- src/ch4/src/useSophus.cpp
+++ src/ch4/src/useSophus.cpp
@@ -4,30 +4,58 @@
 #include <sophus/se3.hpp>
 #include <sophus/so3.hpp>
 
+using namespace std;
+using namespace Eigen;
 
 class SophusExampleNode : public rclcpp::Node {
 public:
     SophusExampleNode() : Node("sophus_example_node") {
-        // 定义一个旋转矩阵
-        Eigen::Matrix3d rotation_matrix = Eigen::AngleAxisd(M_PI / 4, Eigen::Vector3d::UnitZ()).toRotationMatrix();
-        
-        // 使用 Sophus 将旋转矩阵转换为 SO(3) 群
-        Sophus::SO3d SO3_rotation(rotation_matrix);
+        // 沿Z轴转90度的旋转矩阵
+        Matrix3d R = AngleAxisd(M_PI / 2, Vector3d(0, 0, 1)).toRotationMatrix();
+        // 或者四元数
+        Quaterniond q(R);
+        Sophus::SO3d SO3_R(R);              // Sophus::SO3d可以直接从旋转矩阵构造
+        Sophus::SO3d SO3_q(q);              // 也可以通过四元数构造
+        // 二者是等价的
+        cout << "SO(3) from matrix:\n" << SO3_R.matrix() << endl;
+        cout << "SO(3) from quaternion:\n" << SO3_q.matrix() << endl;
+        cout << "they are equal" << endl;
 
-        // 定义一个平移向量
-        Eigen::Vector3d translation(1, 0, 0);
+        // 使用对数映射获得它的李代数
+        Vector3d so3 = SO3_R.log();
+        cout << "so3 = " << so3.transpose() << endl;
+        // hat 为向量到反对称矩阵
+        cout << "so3 hat=\n" << Sophus::SO3d::hat(so3) << endl;
+        // 相对的，vee为反对称到向量
+        cout << "so3 hat vee= " << Sophus::SO3d::vee(Sophus::SO3d::hat(so3)).transpose() << endl;
 
-        // 使用 Sophus 定义 SE(3) 群
-        Sophus::SE3d SE3_transform(SO3_rotation, translation);
+        // 增量扰动模型的更新
+        Vector3d update_so3(1e-4, 0, 0); //假设更新量为这么多
+        Sophus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
+        cout << "SO3 updated = \n" << SO3_updated.matrix() << endl;
 
-        // 输出 SE(3) 群的变换矩阵
-        std::cout << "SE3 Transform Matrix: \n" << SE3_transform.matrix() << std::endl;
+        cout << "*******************************" << endl;
+        // 对SE(3)操作大同小异
+        Vector3d t(1, 0, 0);           // 沿X轴平移1
+        Sophus::SE3d SE3_Rt(R, t);           // 从R,t构造SE(3)
+        Sophus::SE3d SE3_qt(q, t);            // 从q,t构造SE(3)
+        cout << "SE3 from R,t= \n" << SE3_Rt.matrix() << endl;
+        cout << "SE3 from q,t= \n" << SE3_qt.matrix() << endl;
+        // 李代数se(3) 是一个六维向量，方便起见先typedef一下
+        typedef Eigen::Matrix<double, 6, 1> Vector6d;
+        Vector6d se3 = SE3_Rt.log();
+        cout << "se3 = " << se3.transpose() << endl;
+        // 观察输出，会发现在Sophus中，se(3)的平移在前，旋转在后.
+        // 同样的，有hat和vee两个算符
+        cout << "se3 hat = \n" << Sophus::SE3d::hat(se3) << endl;
+        cout << "se3 hat vee = " << Sophus::SE3d::vee(Sophus::SE3d::hat(se3)).transpose() << endl;
 
-        // 进行变换：将一个点通过 SE(3) 变换
-        Eigen::Vector3d point(1, 1, 1);
-        Eigen::Vector3d transformed_point = SE3_transform * point;
-
-        std::cout << "Transformed Point: [" << transformed_point.transpose() << "]" << std::endl;
+        // 最后，演示一下更新
+        Vector6d update_se3; //更新量
+        update_se3.setZero();
+        update_se3(0, 0) = 1e-4;
+        Sophus::SE3d SE3_updated = Sophus::SE3d::exp(update_se3) * SE3_Rt;
+        cout << "SE3 updated = " << endl << SE3_updated.matrix() << endl;
     }
 };
 
